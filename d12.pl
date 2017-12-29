85225a48897cbd87fa0c6cb5fd76c0551550ef31
Gustaf Erikson <gerikson@gmail.com>
2017-12-29 11:59:13 +0100

    part 2 enabled through command line option

--- d12.pl
+++ d12.pl
@@ -1,14 +1,13 @@
 #!/usr/bin/perl 
-# Advent of Code 2015 Day 12 -  complete solution
+# Advent of Code 2015 Day 12 - complete solution
 # Problem link: http://adventofcode.com/2015/day/12
 #   Discussion: http://gerikson.com/blog/comp/Advent-of-Code-2015.html#d12
 #      License: http://gerikson.com/files/AoC2015/UNLICENSE
 ###########################################################
-
 use strict;
 use warnings;
 use JSON;
-
+my $part2 = shift || 0;
 my $sum = 0;
 
 sub traverse {
@@ -17,26 +16,30 @@ sub traverse {
         foreach my $el ( @{$in} ) {
             if ( ref($el) ) {
                 traverse($el);
-            } elsif ( $el =~ m/\d+/ ) {
+            }
+            elsif ( $el =~ m/\d+/ ) {
                 $sum += $el;
             }
         }
-    } elsif ( ref($in) eq 'HASH' ) {
+    }
+    elsif ( ref($in) eq 'HASH' ) {
 
         # need to lookahead if we should even consider this hash
         my $redflag = 0;
         while ( my ( $k, $v ) = each %{$in} ) { $redflag = 1 if $v eq 'red' }
 
         # comment this next line for part 1 solution
-        return if $redflag;
+        return if $redflag and $part2;
         foreach my $key ( keys %{$in} ) {
             if ( ref( $in->{$key} ) ) {
                 traverse( $in->{$key} );
-            } elsif ( $in->{$key} =~ m/\d+/ ) {
+            }
+            elsif ( $in->{$key} =~ m/\d+/ ) {
                 $sum += $in->{$key};
             }
         }
-    } else {    # should not occur according to problem text
+    }
+    else {    # should not occur according to problem text
         $sum += $in if ( $in =~ m/\d+/ );
     }
 }
@@ -48,5 +51,5 @@ my $data      = decode_json($json_text);
 
 traverse($data);
 
-print "Sum: $sum\n";
+print 'Part ', $part2 ? '2' : '1', " sum: $sum\n";
 
