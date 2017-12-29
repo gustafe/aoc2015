8530755d391bd14388ef81394d8ce1e11d756f1f
Gustaf Erikson <gerikson@gmail.com>
2017-12-29 11:59:33 +0100

    part 2 enabled through command line option

--- d13.pl
+++ d13.pl
@@ -1,57 +1,63 @@
-#!/usr/bin/perl
+#! /usr/bin/env perl
 # Advent of Code 2015 Day 13 - complete solution
 # Problem link: http://adventofcode.com/2015/day/13
 #   Discussion: http://gerikson.com/blog/comp/Advent-of-Code-2015.html#d13
 #      License: http://gerikson.com/files/AoC2015/UNLICENSE
 ###########################################################
-
-use strict;
+use 5.016;
 use warnings;
-
+use autodie;
+use List::Util qw/max/;
 use Algorithm::Combinatorics qw(permutations);
+#### INIT - load input data from file into array
+my $testing = 0;
+my @input;
+my $file = $testing ? 'test.txt' : 'input.txt';
+open( my $fh, '<', "$file" );
+while (<$fh>) { chomp; s/\r//gm; push @input, $_; }
 
-my $file = 'input2.txt';
-open F, "<$file" or die "can't open $file: $!\n";
-my $feels;
+### CODE
+my $part2 = shift || 0;
 my $people;
-while (<F>) {
-    chomp;
-    s/\r//gm;
-    my ( $p1, $op, $val, $p2 )
-        = ( $_ =~ m/^(\S+) would (\S+) (\d+) .* (\S+)\.$/ );
-    if ( $op eq 'lose' ) { $val = -$val }
+my $feels;
+while (@input) {
+    my $line = shift @input;
+    my ($p1, $op, $val, $p2 ) = ( $line =~ m/^(\S+) would (\S+) (\d+) .* (\S+)\.$/);
+    $val = -$val if $op eq 'lose';
     $feels->{$p1}->{$p2} = $val;
     $people->{$p1}++;
     $people->{$p2}++;
 }
-close F;
 
-# For part 1, uncomment this, run and append the output to the input file
-# foreach my $p ( sort keys %{$people} ) {
-#     print "Gustaf would gain 0 happiness units by sitting next to $p.\n";
-#     print "$p would gain 0 happiness units by sitting next to Gustaf.\n";
-# }
-# exit 0;
-
+if ($part2) {
+    foreach my $p (keys %{$people}) {
+	$people->{Gustaf}++;
+	$feels->{Gustaf}->{$p} = 0;
+	$feels->{$p}->{Gustaf} = 0;
+    }
+}
 # Generate all permutations
 my @list = keys %{$people};
 
-my $arrangement = permutations( \@list );
-
+my $arrangement = permutations(\@list);
+my $max=0;
 while ( my $arr = $arrangement->next ) {
     my $happiness = 0;
-    my @arr       = @{$arr};    # makes following code a bit easier to write
-    for ( my $idx = 0 ; $idx <= $#arr ; $idx++ ) {
-        if ( $idx == 0 ) {      # start of the list
-            $happiness += (   $feels->{ $arr[$idx] }->{ $arr[ $idx + 1 ] }
-                            + $feels->{ $arr[$idx] }->{ $arr[$#arr] } );
-        } elsif ( $idx == $#arr ) {    # end of the list
-            $happiness += (   $feels->{ $arr[$idx] }->{ $arr[0] }
-                            + $feels->{ $arr[$idx] }->{ $arr[ $idx - 1 ] } );
-        } else {
-            $happiness += (   $feels->{ $arr[$idx] }->{ $arr[ $idx + 1 ] }
-                            + $feels->{ $arr[$idx] }->{ $arr[ $idx - 1 ] } );
-        }
+    my @arr = @{$arr}; # makes following code a bit easier to write
+    for ( my $idx = 0; $idx <= $#arr; $idx++ ) {
+	if ( $idx == 0 ) { # start of the list
+	    $happiness += ($feels->{$arr[$idx]}->{$arr[$idx+1]} +
+			   $feels->{$arr[$idx]}->{$arr[$#arr]} )
+	} elsif ( $idx == $#arr ) { # end of the list
+	    $happiness += ($feels->{$arr[$idx]}->{$arr[0]} +
+			   $feels->{$arr[$idx]}->{$arr[$idx-1]} )
+	} else {
+	    $happiness += ( $feels->{$arr[$idx]}->{$arr[$idx+1]} +
+			    $feels->{$arr[$idx]}->{$arr[$idx-1]} )
+	}
     }
-    print $happiness, ' ', join( ' ', @arr ), "\n";
+    #    print $happiness, ' ', join(' ', @arr), "\n";
+    $max = max($max, $happiness);
 }
+
+say $part2?'2':'1',". happiness change: $max";
