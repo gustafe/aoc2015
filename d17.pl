70d82ec98522309b57984242773af1c329fb6c9c
Gustaf Erikson <gerikson@gmail.com>
2017-12-29 14:07:32 +0100

    added header template

--- d17.pl
+++ d17.pl
@@ -1,23 +1,21 @@
 #!/usr/bin/perl
-# Advent of Code 2015 Day 17 - complete solution
-# Problem link: http://adventofcode.com/2015/day/17
+# Advent of Code 2015 Day 17.pl - complete solution
+# Problem link: http://adventofcode.com/2015/day/17.pl
 #   Discussion: http://gerikson.com/blog/comp/Advent-of-Code-2015.html#d17
 #      License: http://gerikson.com/files/AoC2015/UNLICENSE
 ###########################################################
-
 use strict;
 use warnings;
 use feature qw(say);
 use Algorithm::Combinatorics qw(combinations);
-use List::Util qw(sum);
+use List::Util qw(sum minstr);
 
 my $testing = 0;
-my $file = $testing ? 'test.txt' : 'input.txt';
+my $file = $testing ? 'test.txt' : 'input.txt' ;
 open F, "<$file" or die "can't open file: $!\n";
 
 my $containers;
-
-while (<F>) {
+while ( <F> ) {
     chomp;
     s/\r//gm;
     push @{$containers}, $_;
@@ -28,20 +26,17 @@ my $target = $testing ? 25 : 150;
 my $count = 0;
 my %number_of_containers;
 
-foreach my $k ( 1 .. $#{$containers} - 1 ) {
-    my $iter = combinations( $containers, $k );
+foreach my $k ( 4 .. 8 ) { # why these values?
+    # Inspecting the input, not even the 3 largest elements will sum
+    # to the target, and even the first 9 smallest elements will
+    # exceed it
+    my $iter = combinations($containers, $k);
     while ( my $comb = $iter->next ) {
-        if ( sum( @{$comb} ) == $target ) {
+        if ( sum(@{$comb}) == $target ) {
             $count++;
-            $number_of_containers{ scalar @{$comb} }++;
+            $number_of_containers{scalar @{$comb}}++;
         }
     }
 }
 say "Part 1: $count";
-say "Part 2:";
-say "#    count";
-say "----------";
-
-foreach my $n ( sort { $a <=> $b } keys %number_of_containers ) {
-    printf( "%d % 8d\n", $n, $number_of_containers{$n} );
-}
+say "Part 2: ", $number_of_containers{ minstr( keys %number_of_containers ) };
